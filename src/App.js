85c75a3d93a0684d397fed728b575cda984f5043
Pramod S <pramod082002@gmail.com>
2023-08-16 16:10:28 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Dashboard from './Components/Dashboard';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TestQNA from './Components/TestQNA';
 import { cnEntryTest } from './Data/QNA Entry Tests/ALL_ENTRY_TEST';
+import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
           <Route path="*" element={<h1>404 PAGE NOT FOUND</h1>} />
         </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/test" element={<TestQNA />} />
         <Route path="/test/:moduleName/:subjectName/:testType" element={<TestQNA />} />
       </Routes>
